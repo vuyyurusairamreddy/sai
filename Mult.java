c1c313cc632d7b85a439a37ecb034d3c0ae207a2
vuyyurusairamreddy <117892091+vuyyurusairamreddy@users.noreply.github.com>
2022-12-20 14:01:16 +0530

    Add files via upload

---
+++ Mult.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+ class Mult
+    {
+
+    public static void main(String[] args) 
+       {
+
+        int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+      }
+}
